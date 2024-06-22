8863a54066306ca6c937c1624b00237f87876669
damhuman <anton.dasyuk@gmail.com>
2024-06-23 02:55:26 +0300

    add basic hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    scrollGoerli: {
+      url: process.env.SCROLL_GOERLI_RPC_URL,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
+
+export default config;
